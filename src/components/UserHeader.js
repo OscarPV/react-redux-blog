f600949d8fe496f04544433f178405d3254ebe13
Óscar Pérez Vargas <operez@vsys.com>
2019-07-30 20:22:57 -0500

    Memoization

---
+++ src/components/UserHeader.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { fetchUser } from '../actions';
+
+class UserHeader extends React.Component {
+
+    componentDidMount() {
+        this.props.fetchUser(this.props.userId);
+    }
+
+    render() {
+        const { user } = this.props;
+
+        if (!user) {
+            return null;
+        }
+
+        return (
+            <div className="header">
+                {this.props.user.name}
+            </div>
+        );
+    }
+}
+
+const mapStateToProps = (state, ownProps) => {
+    return { user: state.users.find(user => ownProps.userId === user.id)};
+};
+
+export default connect(mapStateToProps, { fetchUser })(UserHeader);
